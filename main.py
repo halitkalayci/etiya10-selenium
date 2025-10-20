5097ff1ec70109e7d6478b11310b239a8d3e0229
halitklyc <halitenes.kalayci@gmail.com>
2025-10-20 15:06:44 +0300

    .

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 chrome = Chrome()
+wait = WebDriverWait(chrome, 10)
 chrome.get("https://saucedemo.com")
 chrome.maximize_window()
 
-username_input = chrome.find_element(By.ID, "user-name")
+username_input = wait.until( EC.visibility_of_element_located(By.ID, "user-name") ) # ✅ Max 10s elementi bekler ve öyle ilerler 
 username_input.send_keys("standard_user") # bir inputa yazı yazmak için.
 
-password_input = chrome.find_element(By.ID, "password")
+password_input = chrome.find_element(By.ID, "password") # ❌ Yanlış (Elementi beklemez direkt geldi varsayar)
 password_input.send_keys("secret_sauce")
 
 login_button = chrome.find_element(By.CSS_SELECTOR, "[data-test='login-button']")
 login_button.click()
-time.sleep(5)
 
