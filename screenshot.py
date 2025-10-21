365a6ceba87d0bb8b0a159e0923381d8afedfec7
halitklyc <halitenes.kalayci@gmail.com>
2025-10-21 14:35:13 +0300

    add foldering to ss

--- screenshot.py
+++ screenshot.py
@@ -2,6 +2,10 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import os
+
+
 
 driver = Chrome()
 wait = WebDriverWait(driver, 10)
@@ -16,5 +20,9 @@ wait.until( EC.visibility_of_element_located((By.ID, "password"))).send_keys("se
 login_button = wait.until( EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-test='login-button']")))
 login_button.click()
 
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+os.makedirs("screenshots", exist_ok=True) # make directories, exist_ok=True klasör varsa onu kullan,yoksa oluştur.
+
 login_button.screenshot("btn.png")
-driver.save_screenshot("ss.png")
+driver.save_screenshot("screenshots/"+timestamp+"_ss.png")
